f4118c4c6379b122037d9e8891981d3ebf5bb60a
Lua <luamdrs1994@gmail.com>
2024-11-13 15:28:47 -0300

    🛸

--- exercises/02.py
+++ exercises/02.py
@@ -2,12 +2,17 @@
 
 import pandas as pd
 
-dados = {
+df = {
     'Produto': ['Camisa', 'Shorts', 'Calça Jeans', 'Tênis', 'Relógio', 'Boné'],
     'Quantidade': [20, 25, 15, 30, 50, 27],
     'Preço': [39.99, 29.99, 120.00, 99.99, 79.99, 39.99]
 }
 
-estoque_loja = pd.DataFrame(dados)
+estoque_loja = pd.DataFrame(df)
 
+estoque_loja['Valor Total'] = estoque_loja['Quantidade'] * estoque_loja['Preço']
 print(estoque_loja)
+
+valor_total_produtos = estoque_loja['Valor Total'].sum()
+
+print(f'\nValor total dos produtos: R${valor_total_produtos:.2f}')
