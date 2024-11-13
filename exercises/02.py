081b3ccb83f006f7f251f0d13d193627e76cd1b3
Lua <luamdrs1994@gmail.com>
2024-11-13 14:19:15 -0300

    🛸

--- exercises/02.py
+++ exercises/02.py
@@ -1,1 +1,13 @@
+# Criando um Dataframe a partir de um dicionario
 
+import pandas as pd
+
+dados = {
+    'Produto': ['Camisa', 'Shorts', 'Calça Jeans', 'Tênis', 'Relógio', 'Boné'],
+    'Quantidade': [20, 25, 15, 30, 50, 27],
+    'Preço': [39.99, 29.99, 120.00, 99.99, 79.99, 39.99]
+}
+
+estoque_loja = pd.DataFrame(dados)
+
+print(estoque_loja)
