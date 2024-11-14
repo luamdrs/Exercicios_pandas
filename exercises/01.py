0eb9eb6cad7d29023023a949bdd178a902400cce
Lua <luamdrs1994@gmail.com>
2024-11-14 11:14:29 -0300

    🐼

--- exercises/01.py
+++
@@ -1,8 +0,0 @@
-# Criando uma série
-
-import pandas as pd
-
-temp = pd.Series([20, 22, 21, 19, 24, 26, 23],
-                index=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
-
-print(temp)
